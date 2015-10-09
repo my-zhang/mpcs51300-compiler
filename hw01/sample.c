a70e5270857f89025071e00ac9aad96033c74723
my-zhang <zhangmengyu.grad@gmail.com>
2015-10-08 19:17:56 -0500

    sample c source file

---
+++ hw01/sample.c
@@ -0,0 +1,13 @@
+// External function extern 
+int foo2(int x); 
+
+/* Function 
+ * a foo func
+ */
+int foo(int count /*cnt */) 
+{ 
+    int sum = 0.0; // init sum to 0
+    for (int i = 1; i <= count; i++) 
+        sum += foo2(i); 
+    return sum; 
+}
