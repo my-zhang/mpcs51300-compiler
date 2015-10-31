8e2ed09dd77dfa9598c1155a39ba14e3b73a4345
my-zhang <zhangmengyu.grad@gmail.com>
2015-10-31 13:56:40 -0500

    partly done with syms

--- hw02/sample.c
+++ hw02/sample.c
@@ -4,7 +4,12 @@ extern int bar(int x);
 int foo(int count) {
     int sum = 2;
     int i;
-    for(i = 1; i<=count; i++) 
+    for(i = 1; i <= count; i++)
         sum = sum + bar(i);
+
+    for(i = 1; i <= count; i++) {
+        int a = i * i;
+        sum += a;
+    }
     return sum; 
 }
