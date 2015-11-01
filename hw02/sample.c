3c7c73bcd5c3440802b6fd3ec30d544790589c43
my-zhang <zhangmengyu.grad@gmail.com>
2015-11-01 16:16:29 -0600

    handin

--- hw02/sample.c
+++ hw02/sample.c
@@ -2,16 +2,12 @@
 extern int bar(int x);
 
 int foo(int count) {
-    float sum = -2.2;
-    string test = "haha";
+    int sum = 1;
+    string s = "haha";
+
     int i;
-    int a;
     for(i = 1; i <= count; i++)
         sum = sum + bar(i);
 
-    for(i = 1; i <= count; i++) {
-        int a = i * i;
-        sum += a;
-    }
     return sum; 
 }
