c41262425a479ae7b21f3b091c938a3a42394057
my-zhang <zhangmengyu.grad@gmail.com>
2015-10-31 22:13:46 -0500

    add test math

--- hw02/test/test_math.py
+++ hw02/test/test_math.py
@@ -9,17 +9,18 @@ SAMPLE_DIR = os.path.join(TEST_DIR, 'samples')
 
 def test_div():
     src = '''
-            int main {
+            int main() {
                 int i = 4500;
                 int j = -123;
                 printd(i/j);
             }'''
 
-    assert [('FUNC_DEF', ('TYPE', 'int'), 
-                         ('ID', 'main'), 
-                         ('COMP_STATS', 
-                             [('VAR_DEC', ('TYPE', 'int'), [('INIT_ASSIGN', ('ID', 'i'), ('ICONST', '4500'))]), 
-                              ('VAR_DEC', ('TYPE', 'int'), [('INIT_ASSIGN', ('ID', 'j'), ('NEG', ('ICONST', '123')))]), 
-                              ('STAT', ('FUNC_CALL', ('ID', 'printd'), [('DIV', ('ID', 'i'), ('ID', 'j'))]))])) ] == \
+    assert [('FUNC_DEF', 
+                ('TYPE', 'int'), 
+                (('ID', 'main'), None), 
+                ('COMP_STATS', 
+                    [('VAR_DEC', ('TYPE', 'int'), [('INIT_ASSIGN', ('ID', 'i'), ('ICONST', '4500'))]), 
+                     ('VAR_DEC', ('TYPE', 'int'), [('INIT_ASSIGN', ('ID', 'j'), ('NEG', ('ICONST', '123')))]), 
+                     ('STAT', ('FUNC_CALL', ('ID', 'printd'), [('DIV', ('ID', 'i'), ('ID', 'j'))]))]))] == \
             parser.parse(src)
 
